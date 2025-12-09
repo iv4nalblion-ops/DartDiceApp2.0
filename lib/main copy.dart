85f2ab1d9444dbaf72e6fca278b8bebe81196a95
unknown <iv4n.alblion@gmail.com>
2025-12-09 02:33:59 -0500

    initial commit

---
+++ lib/main copy.dart
@@ -0,0 +1,13 @@
+import 'packflutter pub getage:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World!"),
+        ),
+      ),
+    ),
+  );
+}
